8a81743326bf683b7cbab16db105efdccb36125f
NierJerome <2594099901@qq.com>
2021-07-16 14:39:37 +0800

    chore: 給useDebounce添加泛型

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -44,7 +44,7 @@ export const useMount = (callback: () => void) => {
 // }
 
 // 后面用泛型规范类型
-export const useDebounce = (value: unknown, delay?: number): any => {
+export const useDebounce = <V>(value: V, delay?: number) => {
   const [debounceValue, setDebounceValue] = useState(value);
   useEffect(() => {
     const timeout = setTimeout(() => setDebounceValue(value), delay);
